79a481301881199491b4fb26d75dd9c5459bea0d
ACER <salimovdeveloper@gmail.com>
2022-12-21 16:29:41 -0600

    commint

--- app/src/main/java/uz/salimovdeveloper/valyutakurslari/MainActivity.kt
+++ app/src/main/java/uz/salimovdeveloper/valyutakurslari/MainActivity.kt
@@ -2,6 +2,8 @@ package uz.salimovdeveloper.valyutakurslari
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 import uz.salimovdeveloper.valyutakurslari.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +11,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN)
         binding = ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
